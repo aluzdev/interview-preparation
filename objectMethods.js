5f745a92daf514de0fcf5fa2876101469ceef5ba
aluz <aluzdev@gmail.com>
2025-01-02 22:25:16 -0600

    all comments

---
+++ objectMethods.js
@@ -0,0 +1,47 @@
+// Object() constructor
+
+// Object.assign()
+
+// Object.create()
+
+// Object.defineProperties()
+
+// Object.defineProperty()
+
+// Object.entries()
+
+// Object.freeze()
+
+// Object.fromEntries()
+
+// Object.getOwnPropertyDescriptor()
+
+// Object.getOwnPropertyDescriptors()
+
+// Object.getOwnPropertyNames()
+
+// Object.getOwnPropertySymbols()
+
+// Object.getPrototypeOf()
+
+// Object.groupBy()
+
+// Object.hasOwn()
+
+// Object.is()
+
+// Object.isExtensible()
+
+// Object.isFrozen()
+
+// Object.isSealed()
+
+// Object.keys()
+
+// Object.preventExtensions()
+
+// Object.seal()
+
+// Object.setPrototypeOf()
+
+// Object.values()

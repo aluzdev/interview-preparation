a3538263071387fefb0b76a0d3be55fdc61fd9f5
aluz <aluzdev@gmail.com>
2025-01-03 20:43:50 -0600

    progress

--- arrayMethods.js
+++ arrayMethods.js
@@ -1,56 +1,131 @@
-//Array() constructor
+//Array() constructor -- create empty array
+let names = ["pris", "cris"];
+names = Array(names.length);
+console.log("🚀 ~ names:", names);
 
-//Array.from()
+//Array.from() -- convert into array
+console.log(Array.from("foo"));
 
-//Array.fromAsync()
+console.log(Array.from([1, 2, 3], (x) => x + x));
 
-//Array.isArray()
+//Array.isArray() -- determine wether something is an array
+console.log(Array.isArray([1, 3, 5]));
 
-//Array.of()
+//Array.of() -- creates an array in a very inefficient way
+console.log(Array.of("foo", 2, "bar", true));
 
-//Array.prototype.at()
+//Array.prototype.at() --get the value of an array by its index, positive or negative
+const at = [5, 12, 8, 130, 44];
+at.at(-1);
+console.log("🚀 ~ at:", at);
 
-//Array.prototype.concat()
+//Array.prototype.concat() -- merge two arrays
+const concat1 = ["a", "b", "c"];
+const concat2 = ["d", "e", "f"];
+const concat3 = concat1.concat(concat2); //["a", "b", "c", "d", "e", "f"]
+console.log("🚀 ~ concat3:", concat3);
 
-//Array.prototype.copyWithin()
+//Array.prototype.copyWithin() --copy part of an array into itself, at another location -- does not add; replaces.
+const copyWithinList = ["a", "b", "c", "d", "e"];
+const middle = Math.round(copyWithinList.length / 2);
+const r = copyWithinList.copyWithin(middle, 0, 2);
+console.log("🚀 ~ r:", r);
 
-//Array.prototype.entries()
+//Array.prototype.entries() -- for of but with access to the index
+const entriesList = ["a", "b", "c", "d"];
 
-//Array.prototype.every()
+for (let [i, num] of entriesList.entries()) {
+  console.log(i, num);
+}
 
-//Array.prototype.fill()
+//Array.prototype.every() -- tests whether all elements in the array pass the test
+const everyList = [10, 20, 30, 40, 50];
+const everyResult = everyList.every((num) => num < 100);
+console.log("🚀 ~ everyResult:", everyResult);
+
+//Array.prototype.fill() -- fill an array with a value / populate array with a value
+const fillList = Array(5);
+const entireFill = fillList.fill("potato");
+console.log("🚀 ~ entireFill:", entireFill);
+
+const partialFill = fillList.fill("tomato", 3);
+console.log("🚀 ~ partialFill:", partialFill);
 
 //Array.prototype.filter()
+const filterList = ["spray", "elite", "exuberant", "destruction", "present"];
+const filtered = filterList.filter((word) => word.length <= 5);
+console.log("🚀 ~ filtered:", filtered);
 
-//Array.prototype.find()
+//Array.prototype.find() -- returns the first element that tests true
+const findList = ["spray", "elite", "exuberant", "destruction", "present"];
+const found = findList.find((word) => word.length <= 5);
+console.log("🚀 ~ found:", found);
 
-//Array.prototype.findIndex()
+//Array.prototype.findIndex() -- returns the index of the first element that tests true
+const findIndexList = ["spray", "elite", "exuberant", "destruction", "present"];
+const foundIndex = findIndexList.findIndex((word) => word.length <= 5);
 
 //Array.prototype.findLast()
+const findLastList = ["spray", "elite", "exuberant", "destruction", "present"];
+const lastFound = findLastList.findLast((word) => word.length <= 5);
+console.log("🚀 ~ lastFound:", lastFound);
 
 //Array.prototype.findLastIndex()
+const findLastIndexList = [
+  "spray",
+  "elite",
+  "exuberant",
+  "destruction",
+  "present",
+];
+const indexOfLastFound = findLastIndexList.findLastIndex(
+  (word) => word.length <= 5
+);
 
-//Array.prototype.flat()
+//Array.prototype.flat() -- flattens an array to a specified depth
+const flatList = [0, 1, 2, [3, 4, [5, 6], [7, 8], [9, 10, [11, 12]]]];
+const flattened = flatList.flat(Infinity);
+console.log("🚀 ~ flattened:", flattened);
 
-//Array.prototype.flatMap()
+//Array.prototype.forEach() -- executes a function once for each element in the array
+const forEachList = ["a", "b", "c"];
 
-//Array.prototype.forEach()
+forEachList.forEach((element) => console.log(element));
+//Array.prototype.includes() -- returns wether a value exists in the array
+const includesList = [1, 2, 3, 4, 5];
+console.log(includesList.includes(5));
 
-//Array.prototype.includes()
+//Array.prototype.indexOf() -- returns -1 if not found, the index of first appearance otherwise.
+const beasts = ["ant", "bison", "camel", "duck", "bison"];
 
-//Array.prototype.indexOf()
+console.log(beasts.indexOf("bison"));
+console.log(beasts.indexOf("bison", 2)); //specifies searchStart position
 
-//Array.prototype.join()
+//Array.prototype.join() -- transforms into string with given separator
+const joinList = [1, 2, 3, 4];
+console.log(joinList.join());
+console.log(joinList.join(" "));
 
-//Array.prototype.keys()
-
-//Array.prototype.lastIndexOf()
+//Array.prototype.lastIndexOf() -- returns -1 if not found, last index of element found otherwise
+const animals = ["Dodo", "Tiger", "Penguin", "Dodo"];
+const indexOfLastDodo = animals.lastIndexOf("Dodo");
+console.log("🚀 ~ indexOfLastDodo:", indexOfLastDodo);
 
 //Array.prototype.map()
+const mapList = [1, 4, 9, 16];
 
-//Array.prototype.pop()
+// Pass a function to map
+const mapped = mapList.map((x) => x * 2);
+console.log("🚀 ~ mapped:", mapped);
 
-//Array.prototype.push()
+//Array.prototype.pop() -- removes last element and returns it
+const popList = ["broccoli", "cauliflower", "cabbage", "kale", "tomato"];
+console.log(popList.pop());
+
+//Array.prototype.push() -- returns the new length of the array
+const pushList = ["pigs", "goats", "sheep"];
+
+console.log(pushList.push("cows"));
 
 //Array.prototype.reduce()
 
--- stringMethods.js
+++ stringMethods.js
@@ -1,20 +1,118 @@
 // String() constructor
+String(thing);
 
 //String.fromCharCode()
+//returns a string created from the specified sequence of UTF-16 code units
+String.fromCharCode(65, 66, 67); // returns "ABC"
+String.fromCharCode(0x2014); // returns "—"
+String.fromCharCode(0x12014); // also returns "—"; the digit 1 is truncated and ignored
+String.fromCharCode(8212); // also returns "—"; 8212 is the decimal form of 0x2014
+String.fromCharCode(0xd83c, 0xdf03); // Code Point U+1F303 "Night with
+String.fromCharCode(55356, 57091); // Stars" === "\uD83C\uDF03"
+String.fromCharCode(0xd834, 0xdf06, 0x61, 0xd834, 0xdf07); // "\uD834\uDF06a\uD834\uDF07"
 
 //String.fromCodePoint()
+//
+String.fromCodePoint(42); // "*"
+String.fromCodePoint(65, 90); // "AZ"
+String.fromCodePoint(0x404); // "\u0404" === "Є"
+String.fromCodePoint(0x2f804); // "\uD87E\uDC04"
+String.fromCodePoint(194564); // "\uD87E\uDC04"
+String.fromCodePoint(0x1d306, 0x61, 0x1d307); // "\uD834\uDF06a\uD834\uDF07"
+String.fromCodePoint("_"); // RangeError
+String.fromCodePoint(Infinity); // RangeError
+String.fromCodePoint(-1); // RangeError
+String.fromCodePoint(3.14); // RangeError
+String.fromCodePoint(3e-2); // RangeError
+String.fromCodePoint(NaN); // RangeError
 
 // String.raw()
+String.raw`Hi\n${2 + 3}!`;
+// 'Hi\\n5!', the character after 'Hi'
+// is not a newline character,
+// '\' and 'n' are two characters.
+
+String.raw`Hi\u000A!`;
+// 'Hi\\u000A!', same here, this time we will get the
+// \, u, 0, 0, 0, A, 6 characters.
+// All kinds of escape characters will be ineffective
+// and backslashes will be present in the output string.
+// You can confirm this by checking the .length property
+// of the string.
+
+const name = "Bob";
+String.raw`Hi\n${name}!`;
+// 'Hi\\nBob!', substitutions are processed.
+
+String.raw`Hi \${name}!`;
+// 'Hi \\${name}!', the dollar sign is escaped; there's no interpolation.
 
 // String.prototype.at()
+// returns a new String consisting of the single UTF-16 code unit located at the specified offset. This method allows for positive and negative integers. Negative integers count back from the last string character.
+// A function which returns the last character of a given string
+function returnLast(str) {
+  return str.at(-1);
+}
+
+let invoiceRef = "my-invoice01";
+
+console.log(returnLast(invoiceRef)); // '1'
+
+invoiceRef = "my-invoice02";
+
+console.log(returnLast(invoiceRef)); // '2'
 
 // String.prototype.charAt()
+//returns a new string consisting of the single UTF-16 code unit at the given index.
+
+//charAt() always indexes the string as a sequence of UTF-16 code units, so it may return lone surrogates. To get the full Unicode code point at the given index, use String.prototype.codePointAt() and String.fromCodePoint().
+const anyString = "Brave new world";
+console.log(`The character at index 0   is '${anyString.charAt()}'`);
+// No index was provided, used 0 as default
+
+console.log(`The character at index 0   is '${anyString.charAt(0)}'`);
+console.log(`The character at index 1   is '${anyString.charAt(1)}'`);
+console.log(`The character at index 2   is '${anyString.charAt(2)}'`);
+console.log(`The character at index 3   is '${anyString.charAt(3)}'`);
+console.log(`The character at index 4   is '${anyString.charAt(4)}'`);
+console.log(`The character at index 999 is '${anyString.charAt(999)}'`);
+//charAt() may return lone surrogates, which are not valid Unicode characters.
+const str = "𠮷𠮾";
+console.log(str.charAt(0)); // "\ud842", which is not a valid Unicode character
+console.log(str.charAt(1)); // "\udfb7", which is not a valid Unicode character
 
 // String.prototype.charCodeAt()
+// returns an integer between 0 and 65535 representing the UTF-16 code unit at the given index.
+//charCodeAt() always indexes the string as a sequence of UTF-16 code units, so it may return lone surrogates. To get the full Unicode code point at the given index, use String.prototype.codePointAt().
+"ABC".charCodeAt(0); // returns 65
+const strq = "𠮷𠮾";
+console.log(strq.charCodeAt(0)); // 55362, or d842, which is not a valid Unicode character
+console.log(strq.charCodeAt(1)); // 57271, or dfb7, which is not a valid Unicode character
 
 // String.prototype.codePointAt()
+//eturns a non-negative integer that is the Unicode code point value of the character starting at the given index. Note that the index is still based on UTF-16 code units, not Unicode code points.
+"ABC".codePointAt(0); // 65
+"ABC".codePointAt(0).toString(16); // 41
+
+"😍".codePointAt(0); // 128525
+"\ud83d\ude0d".codePointAt(0); // 128525
+"\ud83d\ude0d".codePointAt(0).toString(16); // 1f60d
+
+"😍".codePointAt(1); // 56845
+"\ud83d\ude0d".codePointAt(1); // 56845
+"\ud83d\ude0d".codePointAt(1).toString(16); // de0d
+
+"ABC".codePointAt(42); // undefined
+for (const codePoint of str) {
+  console.log(codePoint.codePointAt(0).toString(16));
+}
+// '1f40e', '1f471', '2764'
+
+[...str].map((cp) => cp.codePointAt(0).toString(16));
+// ['1f40e', '1f471', '2764']
 
 // String.prototype.concat()
+//returns a new string.
 
 // String.prototype.endsWith()
 
